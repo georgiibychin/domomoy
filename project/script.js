35e9e174ab56be3ce74dd1a19b7d14880e954942
georgiibychin <154808013+georgiibychin@users.noreply.github.com>
2023-12-25 16:50:45 +0300

    Delete project directory

--- project/script.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-	$('.unclicked').click(function(e){
-		e.preventDefault();
-		var id = $(this).data('service');
-		$('.container.service').hide();
-		$('.container.service-' + id).show();
-	});
-});
